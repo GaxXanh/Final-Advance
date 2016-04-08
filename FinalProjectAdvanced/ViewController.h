91edc3ab06e5cb51438027564176e0c1ea3518ac
Pham Anh <anhapo11@gmail.com>
2016-04-08 10:18:19 +0700

    Initial Commit

---
+++ FinalProjectAdvanced/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FinalProjectAdvanced
+//
+//  Created by Pham Anh on 4/8/16.
+//  Copyright © 2016 com.gaxxanh.iosFinalProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FinalProjectAdvanced/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FinalProjectAdvanced
+//
+//  Created by Pham Anh on 4/8/16.
+//  Copyright © 2016 com.gaxxanh.iosFinalProject. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
